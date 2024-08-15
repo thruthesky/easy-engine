1032e5da5f5347d447d18d285de5a216adbc81b3
Charles <withcenter.dev3@gmail.com>
2024-08-15 16:40:21 +0800

    add region on index.ts

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,7 +1,13 @@
-
 import * as admin from "firebase-admin";
+import {setGlobalOptions} from "firebase-functions/v2";
+import {Config} from "./config";
 
 admin.initializeApp();
+
+// set region
+setGlobalOptions({
+  region: Config.region,
+});
 
 export * from "./user/user.functions";
 export * from "./messaging/messaging.functions";
