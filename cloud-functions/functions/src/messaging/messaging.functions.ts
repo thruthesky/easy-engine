e71c64fe7ee79e0bdb511fc16c341a4a4832ab1f
Charles <withcenter.dev3@gmail.com>
2024-08-15 13:54:54 +0800

    json parse on body

--- cloud-functions/functions/src/messaging/messaging.functions.ts
+++ cloud-functions/functions/src/messaging/messaging.functions.ts
@@ -1,7 +1,7 @@
-import {onRequest} from "firebase-functions/v2/https";
-import {MessagingService} from "./messaging.service";
-import {logger} from "firebase-functions/v2";
-import {handleHttpError} from "../library";
+import { onRequest } from "firebase-functions/v2/https";
+import { MessagingService } from "./messaging.service";
+import { logger } from "firebase-functions/v2";
+import { handleHttpError } from "../library";
 
 /**
  * Send message with tokens
@@ -12,11 +12,7 @@ export const sendMessage = onRequest(async (request, response) => {
   logger.info("request.query of sendPushNotifications", request.body);
 
   try {
-    const body =
-      typeof request.body === "string" ?
-        JSON.parse(request.body) :
-        request.body;
-    const res = await MessagingService.sendMessage(body);
+    const res = await MessagingService.sendMessage(JSON.parse(request.body));
     response.send(res);
   } catch (e) {
     handleHttpError(e, response);
@@ -32,11 +28,9 @@ export const sendMessageToUids = onRequest(async (request, response) => {
   logger.info("request.query of sendPushNotifications", request.body);
 
   try {
-    const body =
-      typeof request.body === "string" ?
-        JSON.parse(request.body) :
-        request.body;
-    const res = await MessagingService.sendMessageToUids(body);
+    const res = await MessagingService.sendMessageToUids(
+      JSON.parse(request.body)
+    );
     response.send(res);
   } catch (e) {
     handleHttpError(e, response);
@@ -53,11 +47,9 @@ export const sendMessageToSubscription = onRequest(
     logger.info("request.query of sendPushNotifications", request.body);
 
     try {
-      const body =
-        typeof request.body === "string" ?
-          JSON.parse(request.body) :
-          request.body;
-      const res = await MessagingService.sendMessageToSubscription(body);
+      const res = await MessagingService.sendMessageToSubscription(
+        JSON.parse(request.body)
+      );
       response.send(res);
     } catch (e) {
       handleHttpError(e, response);
